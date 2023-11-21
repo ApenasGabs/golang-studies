c40408a564ab31df5f06e03f0abfc36ea63ee4e9
Gabriel Rodrigues <gabers357@gmail.com>
2023-11-21 00:35:02 -0300

    Add car struct and update main function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main()  {
-	println("Hello World")
+type car struct {
+	Model string
+	Color string
+}
+// para declarar, podemos fazer assim:
+// xyz :  car
+func main() {
+	car := car{ // declarando e definindo os valores
+		Model: "Fiat",
+		Color: "blue",
+	}
+	println(car.Model, car.Color)
+	car.Model = "Ford"
+	println(car.Model, car.Color)
 }
