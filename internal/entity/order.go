b1559d7bf70fff26c58309a211a9e25aee7ae0fd
Gabriel Rodrigues <gabers357@gmail.com>
2023-11-21 01:05:36 -0300

    Add order validation function

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -1,5 +1,7 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	Price      float64
@@ -16,4 +18,17 @@ func NewOrder(id string, price, tax float64) *Order {
 		Price: price,
 		Tax:   tax,
 	}
+}
+
+func (o *Order) Validate() error {
+	if o.ID == "" {
+		return errors.New("the ID field can not be empty")
+	}
+	if o.Price <= 0 {
+		return errors.New("the Price field must be greater than zero")
+	}
+	if o.Tax < 0 {
+		return errors.New("the Tax field can not be negative")
+	}
+	return nil
 }
